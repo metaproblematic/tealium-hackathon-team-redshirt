3c4c0d19e96fc1caee7d9269d0923576cbfc760e
Yi Zu <zuyi@bu.edu>
2019-08-18 14:19:45 -0700

    Added logo and barchart for rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 //** COMPONENTS  **//
 
-import Data from './components/Data'
+import Data from "./components/Data";
+import BarChart from "./components/BarChart";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-     <Data />
+      {/* <Data /> */}
+      <React.Fragment>
+        <NavBar />
+        <BarChart />
+      </React.Fragment>
     </>
   );
 }
