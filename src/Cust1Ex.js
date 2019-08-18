dab0b427e98c2915b04df044d70f9a4c2654a0c8
Yi Zu <zuyi@bu.edu>
2019-08-18 14:18:58 -0700

    Sample json for one customer

---
+++ src/Cust1Ex.js
@@ -0,0 +1,70 @@
+[
+    {
+audiences: [
+            "Frequent Visitor",
+            "VIP"
+        ],
+audiences_joined_at: {
+Frequent Visitor: "2019-04-23T12:20:10.250Z",
+VIP: "2019-04-11T16:24:30.200Z"
+        },
+badges: [
+            "Trend Follower",
+            "Fan",
+            "Returning Customer"
+        ],
+dates: {
+First visit: 1550257892000,
+Last visit: 1562341900000,
+Last purchase: 1562341800000
+        },
+flags: {
+Returning visitor: true,
+Has purchased > 5 items: true,
+Has purchased > 10 items: false,
+Is subsribed to newsletter: false
+        },
+metrics: {
+lat: 32.97965018403566,
+long: -117.1701239400667,
+Average Purchase Value: 115.33333333333333,
+Average Value Per Visit: 45.130434782608695,
+Lifetime total value: 1038,
+Lifetime purchases: 9,
+Days since last visit: 4,
+Discount page views: 20,
+New arrival page views: 15,
+Hat page views: 3,
+Jeans page views: 17,
+Accesories page views: 14,
+Lifetime visits: 23,
+Lifetime Direct Visits: 16,
+Lifetime Referred Visits: 7,
+Lifetime events: 95,
+Total time spent on site in minutes: 194,
+Number of logins - Past 90 days: 2,
+Future Lifetime Value 12 Months: 1490.8419465548918,
+Future Lifetime Value: 2108.8799458400836
+        },
+properties: {
+Lifetime browser types used (favorite): "IE",
+Lifetime devices used (favorite): "Mac",
+Favorite Top Level Category: "Men",
+Email: "",
+visitor_id: "afde3664-a799-4bfe-ad9d-acf4103e5b76"
+        },
+current_visit: {
+flags: {
+Is Direct Visit: true
+            },
+properties: {
+Active Browser: "IE",
+Active Device: "iPhone",
+Entry URL: "http://www.acme.com/returns"
+            },
+dates: {
+Entry Time: 1566152933415
+            }
+        }
+    }
+]
